cbb4f225fc627a152c4f0abc5153e3dca0cd4d8f
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-12-27 16:43:37 -0300

    new env

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
-require('dotenv/config')
+//require('dotenv/config')
 
 mongoose.connect(process.env.DB_CLUSTER)
+//console.log(process.env.DB_CLUSTER)
 
 mongoose.Promise = global.Promise
 
