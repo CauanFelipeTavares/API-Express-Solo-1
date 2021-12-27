e8b7f8e3eb367e2117f960dbdeebd0d3a40e4b34
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-12-27 03:22:43 -0300

    database

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv/config')
 
-mongoose.connect(process.env.DB)
+mongoose.connect("mongodb+srv://Calires:felipe23@cluster0.v9dm3.mongodb.net/express1")
 
 mongoose.Promise = global.Promise
 
