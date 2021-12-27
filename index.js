2392775c89abbe794fa45aebf944230d7535cf49
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-12-27 04:08:35 -0300

    cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 require('dotenv/config')
+var cors = require('cors')
 
 const app = express()
+
+app.use(cors())
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
