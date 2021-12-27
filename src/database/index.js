4a31d73f3a7278732e60cfe3b5ed6b4cba762fcd
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-12-27 16:45:03 -0300

    desisti do .env

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 //require('dotenv/config')
 
-mongoose.connect(process.env.DB_CLUSTER)
+mongoose.connect("mongodb+srv://Calires:felipe23@cluster0.v9dm3.mongodb.net/express1")
 //console.log(process.env.DB_CLUSTER)
 
 mongoose.Promise = global.Promise
