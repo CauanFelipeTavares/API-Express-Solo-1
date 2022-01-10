b3e0e1265b6d489895aa51944b46d0ad49dd9eef
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2022-01-10 15:56:35 -0300

    .env

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 //require('dotenv/config')
 
-mongoose.connect("mongodb+srv://Calires:felipe23@cluster0.v9dm3.mongodb.net/express1")
+mongoose.connect(`mongodb+srv://Calires:${process.env.DB_CLUSTER}@cluster0.v9dm3.mongodb.net/express1`)
 //console.log(process.env.DB_CLUSTER)
+//https://express-test-one-calires.herokuapp.com
 
 mongoose.Promise = global.Promise
 
